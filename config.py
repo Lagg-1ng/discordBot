f7b187dbe1c00f3bc3e412d720cd5fd63d978107
Arseniy <67089440+Lagg-1ng@users.noreply.github.com>
2023-10-13 03:19:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'token': 'MTE2MTgwMjc3Nzc0NDM4NDAzMA.GFF4wn.FpudheNNpC9tfO0e6IC8Fx2ByYROWmEIT2MFDc',
+    'token': '',
     'bot': 'events_bot',
-    'client_id': '1161802777744384030',
+    'client_id': '',
 }
 
